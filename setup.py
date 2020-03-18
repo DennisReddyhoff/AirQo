792ddd863deb02d31a27ec2f00d9b20e073fa4f3
Dennis Reddyhoff <d.reddyhoff@sheffield.ac.uk>
2020-03-18 13:38:54 +0000

    Restructure package

--- setup.py
+++ setup.py
@@ -1,22 +1,13 @@
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="get_dataframe", # Replace with your own username
-    version="0.0.1",
-    author="Dennis Reddyhoff",
-    author_email="d.reddyhoff@sheffield.ac.uk",
-    description="Package to pull down and cache data from AirQo sensor APIs",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/DennisReddyhoff/AirQo",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+from distutils.core import setup
+setup(
+  name = 'get_dataframe',
+  packages = ['get_dataframe'], # this must be the same as the name above
+  version = '1.0',
+  description = 'Download and cache data from AirQo sensors',
+  author = 'Dennis Reddyhoff',
+  author_email = 'd.reddyhoff@sheffield.ac.uk',
+  url = 'https://github.com/DennisReddyhoff/AirQo.git',
+  keywords = ['airqo'],
+  classifiers = [],
+  install_requires=['requests', 'json', 'pandas', 'time', 'datetime', 'dateutil.parser', 'fnmatch'],
 )
